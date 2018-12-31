7eaeb7762d6df12c4344be0fb6912661e671c9d4
Adam Smith <amsmith@wmp.com>
2018-12-31 15:51:47 -0600

    Text as title Mine cleanup

--- src/commands/mine.js
+++ src/commands/mine.js
@@ -12,14 +12,6 @@ const msgDefaults = {
     icon_emoji: config('ICON_EMOJI')
 }
 
-// let attachments = [
-//     {
-//         title: `You have ## 🌯\'s`,
-//         color: '#2FA44F',
-//         mrkdwn_in: ['text']
-//     }
-// ]
-
 const handler = (payload, res) => {
     console.log('mine command')
 
@@ -38,9 +30,8 @@ const handler = (payload, res) => {
                         channel: payload.channel_name,
                         attachments: [
                             {
-                                title: `🌯 Leaderboard`,
+                                title: `You have ${totalBurritos} burritos`,
                                 color: '#2FA44F',
-                                text: `You have ${totalBurritos} burritos`,
                                 mrkdwn_in: ['text']
                             }
                         ]
@@ -63,43 +54,6 @@ const handler = (payload, res) => {
     }
 
     getMine();
-
-/*
-    // Query for # of burritos by user ID
-    function getAttachments() {
-        let mineQuery = `SELECT total_burritos FROM burritos_by_user` +
-                ` WHERE user_id = '${payload.user_id}';`
-        console.log('mineQuery: '+mineQuery)
-
-        connection.query(mineQuery, (err, rows, fields) => {
-            // TODO: send error message
-            if (err) throw err
-            console.log('rows[0]')
-            console.log(rows[0])
-            console.log('rows[0].total_burritos')
-            console.log(rows[0].total_burritos)
-            if(typeof rows[0] !== 'undefined') {
-                let totalBurritos = rows[0].total_burritos
-                console.log(payload.user_id, ' has this many burritos: ', totalBurritos)
-                return {
-                    title: `You have ${totalBurritos} 🌯\'s`,
-                    color: '#2FA44F',
-                    mrkdwn_in: ['text']
-                }
-            }
-        })
-    }
-
-    let msg = _.defaults({
-        channel: payload.channel_name,
-        attachments: getAttachments()
-    }, msgDefaults)
-
-    res.set('content-type', 'application/json')
-    res.status(200).json(msg)
-    console.log('msg ', msg)
-    return
-*/
 }
 
 module.exports = { pattern: /mine/ig, handler: handler }
