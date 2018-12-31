cfc5c54e0a9b3c850ee4f68bc721b9416bf1f555
Adam Smith <amsmith@wmp.com>
2018-12-31 15:02:24 -0600

    Promises

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -15,48 +15,43 @@ const msgDefaults = {
 const handler = (payload, res) => {
     console.log('leaderboard command')
 
-    // Query for top 10 # of burritos by user ID
-    let msg = _.defaults({
-        channel: payload.channel_name,
-        attachments: new Promise(function(resolve, reject) {
-            console.log('getAttachments')
+    let getAttachments = new Promise(
+        (resolve, reject) => {
+            console.log(1)
             let leaderboardQuery = `SELECT user_id, total_burritos FROM burritos_by_user` +
-            ` WHERE total_burritos > 0 ORDER BY total_burritos DESC, user_id DESC LIMIT 10;`
+                    ` WHERE total_burritos > 0 ORDER BY total_burritos DESC, user_id DESC LIMIT 10;`
             console.log('leaderboardQuery: '+leaderboardQuery)
             connection.query(leaderboardQuery, (err, rows, fields) => {
                 // TODO: send error message
-                if (err) throw err
+                if (err) reject(err)
                 console.log('rows ', rows)
                 let res = {
                     title: `🌯 leaderboard`,
                     color: '#2FA44F',
-                    text: new Promise(function(resolve, reject) {
-                        console.log('getText')
-                        console.log('getText for ', rows)
-                        let text = ''
-                        for(let i = 0; i < 10; i++) {
-                            if(typeof rows[i] !== 'undefined') {
-                                let row = rows[i]
-                                console.log('row ', row)
-                                text += `#${i + 1}: <@${row.user_id}> - ${row.total_burritos}\n`
-                            }
-                        }
-                        console.log('text ',text)
-                        resolve(text)
-                    }),
+                    text: 'new Promise(getText(rows))',
                     mrkdwn_in: ['text']
                 }
                 resolve(res)
             })
-        })
+        }
+    )
+
+    getAttachments().then(console.log(2))
+
+
+/*
+    // Query for top 10 # of burritos by user ID
+    let msg = _.defaults({
+        channel: payload.channel_name,
+        attachments: new Promise(getAttachments(res, rej))
     }, msgDefaults)
 
     console.log('after msg')
-/*
+
     function getAttachments() {
         console.log('getAttachments')
         let leaderboardQuery = `SELECT user_id, total_burritos FROM burritos_by_user` +
-        ` WHERE total_burritos > 0 ORDER BY total_burritos DESC, user_id DESC LIMIT 10;`
+                ` WHERE total_burritos > 0 ORDER BY total_burritos DESC, user_id DESC LIMIT 10;`
         console.log('leaderboardQuery: '+leaderboardQuery)
         connection.query(leaderboardQuery, (err, rows, fields) => {
             // TODO: send error message
@@ -79,6 +74,7 @@ const handler = (payload, res) => {
         console.log('getText')
         console.log('getText for ', rows)
         let text = ''
+        let i = 0;
         for(let i = 0; i < 10; i++) {
             if(typeof rows[i] !== 'undefined') {
                 let row = rows[i]
@@ -89,17 +85,15 @@ const handler = (payload, res) => {
         console.log('text ',text)
         resolve(text)
     }
-    console.log('after getText declared')
-*/
 
-    setTimeout(send, 5000)
-    function send() {
-        res.set('content-type', 'application/json')
-        res.status(200).json(msg)
-        console.log('msg ', msg)
-        console.log('msg.attachments ', msg.attachments)
-        return
-    }
+    console.log('after getText declared')
+
+    res.set('content-type', 'application/json')
+    res.status(200).json(msg)
+    console.log('msg ', msg)
+    console.log('msg.attachments ', msg.attachments)
+    return
+*/
 }
 
 module.exports = { pattern: /leaderboard/ig, handler: handler }
