082d4687394c7edea32671286f9059fcf02a46b0
Adam Smith <amsmith@wmp.com>
2019-01-07 13:24:52 -0600

    Remove lottery attempt Fix leaderboard sorting

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -18,7 +18,7 @@ const handler = (payload, res) => {
     const getAttachments = new Promise(
         (resolve, reject) => {
             let leaderboardQuery = `SELECT user_id, total_burritos FROM burritos_by_user` +
-            ` WHERE total_burritos > 0 ORDER BY total_burritos DESC, user_id DESC LIMIT 10;`
+            ` WHERE total_burritos > 0 ORDER BY total_burritos DESC DESC LIMIT 10;`
             console.log('leaderboardQuery: '+leaderboardQuery)
             connection.query(leaderboardQuery, (err, rows, fields) => {
                 // TODO: send error message
