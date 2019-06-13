fd7b275332fa096236c37927f6e5807a622f662c
Adam Smith <amsmith@wmp.com>
2019-06-12 21:31:09 -0500

    Drawing is live

--- src/commands/drawing.js
+++ src/commands/drawing.js
@@ -7,7 +7,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL)
 
 const msgDefaults = {
-    response_type: 'ephemeral',
+    response_type: 'in_channel',
     username: 'Yo Burrito',
     icon_emoji: config('ICON_EMOJI')
 }
