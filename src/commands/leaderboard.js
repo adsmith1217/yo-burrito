af82a79a430a1e5c1fffd7476d34eddfac040072
Adam Smith <amsmith@wmp.com>
2019-06-25 14:46:17 -0500

    Leaderboard for 50

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -18,7 +18,7 @@ const handler = (payload, res) => {
     const getAttachments = new Promise(
         (resolve, reject) => {
             let leaderboardQuery = `SELECT user_id, total_burritos FROM burritos_by_user` +
-            ` WHERE total_burritos > 0 ORDER BY total_burritos DESC LIMIT 30;`
+            ` WHERE total_burritos > 0 ORDER BY total_burritos DESC LIMIT 50;`
             console.log('leaderboardQuery: '+leaderboardQuery)
             connection.query(leaderboardQuery, (err, rows, fields) => {
                 // TODO: send error message
@@ -31,7 +31,7 @@ const handler = (payload, res) => {
     const getFormattedText = function(rows) {
         console.log('getText for ', rows)
         let text = ''
-        for(let i = 0; i < 30; i++) {
+        for(let i = 0; i < 50; i++) {
             if(typeof rows[i] !== 'undefined') {
                 let row = rows[i]
                 text += `#${i + 1}: <@${row.user_id}> - ${row.total_burritos}\n`
