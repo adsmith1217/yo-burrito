f0318a4a74628da24f73f378b88afeefb17f6e5c
Alex Schach <aschach@wmp.com>
2019-08-27 12:17:20 -0500

    more logging for daily-reset

--- src/tasks/daily-reset.js
+++ src/tasks/daily-reset.js
@@ -1,17 +1,35 @@
 
 'use strict'
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL)
+const mysql = require('mysql');
+let connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+console.log('console log - daily reset scheduled job');
 
-console.log('daily reset scheduled job')
+let dailyResetQuery = `UPDATE burritos_by_user SET daily_allowance = 5;`;
+console.log('console log - dailyResetQuery', dailyResetQuery);
 
-let dailyResetQuery = `UPDATE burritos_by_user SET daily_allowance = 5;`
-console.log('dailyResetQuery', dailyResetQuery)
+connection.on('close', function (err) {
+    console.log('console log - connection closed');
+});
+
+connection.on('error', function (err) {
+    console.log('console log - connection error: ' + err);
+});
 
 connection.query(dailyResetQuery, (err, rows, fields) => {
-    if (err) throw err
-    console.log('daily reset success')
+    if (err) {
+        console.log('console log - error code: ' + err.code);
+        throw err;
+    }
+    console.log('console log - daily reset success');
 })
 
-return
+// connection.end(function (err) {
+//     if (err) {
+//         console.log('console log - error code: ' + err.code);
+//         throw err;
+//     }
+//     // The connection is terminated now
+// });
+
+return;
