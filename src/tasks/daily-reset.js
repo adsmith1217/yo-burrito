a7024340e8f6bd74f8fe5a41ded04e0e424f5c62
Alex Schach <aschach@wmp.com>
2019-09-09 20:21:08 -0500

    update logging, try ending in bot.js

--- src/tasks/daily-reset.js
+++ src/tasks/daily-reset.js
@@ -9,16 +9,16 @@ let dailyResetQuery = `UPDATE burritos_by_user SET daily_allowance = 5;`;
 console.log('console log - dailyResetQuery', dailyResetQuery);
 
 connection.on('close', function (err) {
-    console.log('console log - connection closed');
+    console.log('console log - connection closed daily-reset');
 });
 
 connection.on('error', function (err) {
-    console.log('console log - connection error: ' + err);
+    console.log('console log - connection error daily-reset: ' + err);
 });
 
 connection.query(dailyResetQuery, (err, rows, fields) => {
     if (err) {
-        console.log('console log - error code: ' + err.code);
+        console.log('console log - error dailyResetQuery: ' + err.code);
         throw err;
     }
     console.log('console log - daily reset success');
@@ -26,10 +26,10 @@ connection.query(dailyResetQuery, (err, rows, fields) => {
 
 connection.end(function (err) {
     if (err) {
-        console.log('console log - error code: ' + err.code);
+        console.log('console log - error ending connection daily-reset: ' + err.code);
         throw err;
     }
-    console.log('console log - connection ended on purpose');
+    console.log('console log - connection ended on purpose daily-reset');
     // The connection is terminated now
 });
 
